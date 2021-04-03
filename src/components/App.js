1d5eece01d87d18ff670c8d3e43a65d1ff6fe47d
장재영 <jaey5oung@naver.com>
2021-04-03 16:33:25 +0900

    4/3 trello action,reducers,store init setting

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import TrelloList from './TrelloList';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+      <TrelloList title="test"/>
+    </div>
+  );
+}
+
+export default App;
